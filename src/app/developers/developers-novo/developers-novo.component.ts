cabf9b1f24a8d93666a240f5bc37fb7344344f30
Marcus Veloso <desenvolvedoroficial@gmail.com>
2021-11-05 19:53:26 -0300

    formatar campos novo developer

--- src/app/developers/developers-novo/developers-novo.component.ts
+++ src/app/developers/developers-novo/developers-novo.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
+import { FormBuilder, FormControlName, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { fromEvent, merge, Observable } from 'rxjs';
 import { DeveloperService } from 'src/app/services/developer.service';
 import { DisplayMessage, GenericValidator, ValidationMessages } from 'src/app/utils/generic-form-validation';
+
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 import { Developer } from '../models/developer';
+
+export interface sexo {
+  id: string,
+  name: string
+}
 
 @Component({
   selector: 'app-developers-novo',
@@ -12,6 +20,15 @@ import { Developer } from '../models/developer';
 })
 export class DevelopersNovoComponent implements OnInit {
 
+  sexoOpcoesLista: sexo[] = [
+    {id: '1', name: 'Masculino'},
+    {id: '2', name: 'Feminino'},
+    {id: '3', name: 'Não desejo informar'},
+    {id: '4', name: 'Outros'},
+  ];
+
+  @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
+
   errors: any[] = [];
   developerForm: FormGroup;
   developer: Developer = new Developer();
@@ -19,6 +36,10 @@ export class DevelopersNovoComponent implements OnInit {
   validationMessages: ValidationMessages;
   genericValidator: GenericValidator;
   displayMessage: DisplayMessage = {};
+
+  colorTheme = 'theme-dark-blue'; 
+  bsConfig?: Partial<BsDatepickerConfig>;
+  datanascimento: Date = new Date();
 
   constructor(private fb: FormBuilder,
     private developerService: DeveloperService,
@@ -44,6 +65,7 @@ export class DevelopersNovoComponent implements OnInit {
       };
 
       this.genericValidator = new GenericValidator(this.validationMessages);
+      this.bsConfig = Object.assign({}, { containerClass: this.colorTheme });
     }
 
   ngOnInit(): void {
@@ -56,6 +78,15 @@ export class DevelopersNovoComponent implements OnInit {
       hobby: ['', [Validators.required]],
       datanascimento: ['', [Validators.required]],
     });
+  }
+
+  ngAfterViewInit(){
+    let controlBlurs: Observable<any>[] = this.formInputElements
+    .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
+
+    merge(...controlBlurs).subscribe(()=>{
+      this.displayMessage = this.genericValidator.processarMensagens(this.developerForm);      
+    })
   }
 
   gravar(){
