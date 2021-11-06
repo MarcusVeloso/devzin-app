e5a78581011c641d4a3992de168d3cc2ff60b603
Marcus Veloso <desenvolvedoroficial@gmail.com>
2021-11-06 17:50:48 -0300

    add menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'devzin-app';
+
+  public isCollapsed: boolean;
+
+  constructor() {
+    this.isCollapsed = true;
+  }
 }
